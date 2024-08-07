2452c94e5779c3ee5e9672626c5aa2d740d87ba3
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-08-07 22:10:14 +0100

    added logs for each api endpoint in the console

--- src/main/java/com/dominion/calculator/controller/CalculatorController.java
+++ src/main/java/com/dominion/calculator/controller/CalculatorController.java
@@ -1,5 +1,7 @@
 package com.dominion.calculator.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import com.dominion.calculator.service.CalculatorService;
@@ -7,6 +9,8 @@ import com.dominion.calculator.service.CalculatorService;
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CalculatorController.class);
 
     private final CalculatorService calculatorService;
 
@@ -16,33 +20,39 @@ public class CalculatorController {
 
     @GetMapping("/add")
     public double add(@RequestParam double x, @RequestParam double y) {
+        logger.info("Received request to add {} and {}", x, y);
         return calculatorService.add(x, y);
     }
 
     @GetMapping("/subtract")
     public double subtract(@RequestParam double x, @RequestParam double y) {
+        logger.info("Received request to subtract {} from {}", y, x);
         return calculatorService.subtract(x, y);
     }
 
 
     @GetMapping("/multiply")
     public double multitply(@RequestParam double x, @RequestParam double y) {
+        logger.info("Received request to multiply {} and {}", x, y);
         return calculatorService.multiply(x, y);
     }
 
     @GetMapping("/divide")
     public double divide(@RequestParam double x, @RequestParam double y) {
+        logger.info("Received request to divide {} by {}", x, y);
         return calculatorService.divide(x, y);
     }
 
     @GetMapping("/power")
     public double power(@RequestParam double base, @RequestParam double exponent) {
+        logger.info("Received request to raise {} to the power of {}", base, exponent);
         return calculatorService.power(base, exponent);
     }
 
 
     @GetMapping("/sqrt")
     public double sqrt(@RequestParam double x) {
+        logger.info("Received request to square {}", x);
         return calculatorService.sqrt(x);
     }
 
--- src/main/java/com/dominion/calculator/service/CalculatorService.java
+++ src/main/java/com/dominion/calculator/service/CalculatorService.java
@@ -1,38 +1,57 @@
 package com.dominion.calculator.service;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class CalculatorService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CalculatorService.class);
+
     public double add(double x, double y) {
-        return x + y;
+        double result = x + y;
+        logger.info("Adding {} and {}: {}" , x, y, result);
+        return result;
     }
 
     public double subtract(double x, double y) {
-        return x - y;
+        double result = x - y;
+        logger.info("Subtracting {} from {}: {}" , y, x, result);
+        return result;
     }
 
     public double multiply(double x, double y) {
-        return x * y;
+        double result = x * y;
+        logger.info("Multiplying {} and {}: {}" , x, y, result);
+        return result;
     }
 
     public double divide(double x, double y) {
         if(y == 0) {
+            logger.error("Can't divide by zero");
             throw new IllegalArgumentException("Can't divide by zero");
         }
 
-        return x / y;
+        double result = x / y;
+        logger.info("Dividing {} by {}: {}", x, y, result);
+        return result;
     }
 
     public double power(double base, double exponent) {
-        return Math.pow(base, exponent);
+        double result = Math.pow(base, exponent);
+        logger.info("Calculating {} to the power of {}: {}", base, exponent, result);
+        return result;
     }
 
     public double sqrt(double a) {
         if(a < 0) {
+            logger.error("Can't square a negative number " + "Try the square root of " + (a * -1));
             throw new IllegalArgumentException("Can't square a negative number " + "Try the square root of " + (a * -1));
         }
-        return Math.sqrt(a);
+        double result = Math.sqrt(a);
+        logger.info("Calculating the square of {}: {}", a, result);
+        return result;
     }
 }
