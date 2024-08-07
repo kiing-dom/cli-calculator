3f49b1e50ac5360ef7a5e29d0cacf6657abab562
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-08-07 15:26:42 +0100

    created calculator controller

---
+++ src/main/java/com/dominion/calculator/controller/CalculatorController.java
@@ -0,0 +1,45 @@
+package com.dominion.calculator.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.dominion.calculator.service.CalculatorService;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    private CalculatorService calculatorService;
+
+    @GetMapping("/add")
+    public double add(@RequestParam double x, double y) {
+        return calculatorService.add(x, y);
+    }
+
+    @GetMapping("/subtract")
+    public double subtract(@RequestParam double x, double y) {
+        return calculatorService.subtract(x, y);
+    }
+
+
+    @GetMapping("/multitply")
+    public double multitply(@RequestParam double x, double y) {
+        return calculatorService.multiply(x, y);
+    }
+
+    @GetMapping("/divide")
+    public double divide(@RequestParam double x, double y) {
+        return calculatorService.divide(x, y);
+    }
+
+    @GetMapping("/power")
+    public double power(@RequestParam double base, double exponent) {
+        return calculatorService.power(base, exponent);
+    }
+
+
+    @GetMapping("/sqrt")
+    public double sqrt(@RequestParam double x) {
+        return calculatorService.sqrt(x);
+    }
+
+}
